973d8a950d3f6709cb03bcfab46a2f5c053b765e
pylnata <pnataliya@icloud.com>
2019-10-05 08:19:23 +0200

    responsive design fix

--- src/Game/options.js
+++ src/Game/options.js
@@ -3,7 +3,11 @@ export const height = window.innerHeight < 600 ? window.innerHeight : 600;
 
 // 600  - 210
 // height - x
-export const groundHeight = height * 210 / 600 * 0.8;
+
+let fix = 0;
+if (height >=360 && height <= 375) fix = -20;
+export const groundHeight = height * 210 / 600 * 0.8 + fix;
+
 export const groundY = height - groundHeight ;
 
 export const playerX = Math.round(width / 3.5);
@@ -11,13 +15,19 @@ export const playerY = groundY + 20;
 
 //800 = .3
 //height - x
-export const playerScale = (Math.round(width * 3/10)/800);
+
+if (height >=360 && height <= 375) fix = -0.05;
+
+export const playerScale = (Math.round(width * 3/10)/800) + fix;
 // 800 - 120
 // width - x
 export const bananaWidth = Math.round((width * 80) / 800);
 export const snailWidth = Math.round((width * 120) / 800);
 
+
+
 export const snailY = playerY - snailWidth + 10;
 
-export const bananaY = playerY - 120 - 2.4 * bananaWidth;
+export const bananaY = playerY - 100 - 2.4 * bananaWidth;
+
 
