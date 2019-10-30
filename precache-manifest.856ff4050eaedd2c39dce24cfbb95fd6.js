66d9eb30a924108c29a5ac000d740f89131509f5
pylnata <pnataliya@icloud.com>
2019-10-30 13:36:59 +0100

    Updates

---
+++ precache-manifest.856ff4050eaedd2c39dce24cfbb95fd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af22a842949615717959b285a7d3e66",
+    "url": "/mowgli/index.html"
+  },
+  {
+    "revision": "4471dac0a7ded8c95be2",
+    "url": "/mowgli/static/css/main.bab743a8.chunk.css"
+  },
+  {
+    "revision": "1effcc24bd0a21ec993b",
+    "url": "/mowgli/static/js/2.334677e5.chunk.js"
+  },
+  {
+    "revision": "4471dac0a7ded8c95be2",
+    "url": "/mowgli/static/js/main.e92b9b01.chunk.js"
+  },
+  {
+    "revision": "411ee10c342b49d0bfc4",
+    "url": "/mowgli/static/js/runtime-main.54d8cccc.js"
+  }
+]);
